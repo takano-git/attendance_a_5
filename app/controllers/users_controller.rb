cd52482eaf220688675455096394b3331618d1b2
takano-git <p.aaattt25@gmail.com>
2019-11-06 01:46:45 +0000

    パーシャル化したヴァリデーションエラーメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    @user= User.new(user_params)
+    if@user.save
+      #保存に成功した処理
+    else
+      render :new
+    end
+  end
+  
   def edit
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
