649b459e8d36e23720c78339cf75089a335dccd0
takano-git <p.aaattt25@gmail.com>
2019-11-06 03:28:51 +0000

    Finifh signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new
   end
@@ -6,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user= User.new(user_params)
     if@user.save
-      #保存に成功した処理
+      flash[:success] ='新規作成に成功しました。'
+      redirect_to @user
     else
       render :new
     end
