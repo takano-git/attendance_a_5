58997fb30ab6fbdd44d7b7ae43653a574c863ca0
takano-git <p.aaattt25@gmail.com>
2019-11-11 21:45:26 +0000

    ダイジェスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,18 @@ class User < ApplicationRecord
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  # 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost =
+    if ActiveModel::SecurePassword.min_cost
+      BCrypt::Enggin::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+  BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返します。
+  
 end
