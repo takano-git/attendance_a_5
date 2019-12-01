694d4a2b9aeb15ba730f3bc062434577db1bb4a0
takano-git <p.aaattt25@gmail.com>
2019-12-01 21:22:44 +0000

    退勤ボタン表示追加

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,13 @@
 module AttendancesHelper
+  def attendance_state(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    # if文は条件式がtrueなら処理を実行し、falseならなにも処理を実行しない。つまりボタンは表示されない。
+    if  Date.current == attendance.worked_on
+      return '出勤' if attendance.started_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    # Rubyではメソッドの最後の式のreturnを省略できるようだ。
+    false
+  end
 end
