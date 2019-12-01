a449d588d0d489016a4ef8abb66cccb5b1191891
takano-git <p.aaattt25@gmail.com>
2019-12-01 13:05:51 +0000

    時間と分の表示追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,15 @@ class AttendancesController < ApplicationController
   
   def update
     @user = User.find(params[:user_id])
-    @attendance = Ateendance.find(params[:id])
-    # 
+    @attendance = Attendance.find(params[:id])
+    # 出勤時間が未登録であることを判定します。
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec:0))
+        flash[:info] = "おはようございます！"
+      else
+        flash[:dander] = "勤怠登録に失敗しました。やり直してください。"
+      end
+    end
+    redirect_to @user
   end
 end
