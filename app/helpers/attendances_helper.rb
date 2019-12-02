142fd9d07c0fda0f82243918bf60405b7abfee36
takano-git <p.aaattt25@gmail.com>
2019-12-02 11:53:59 +0000

    在社時間表示機能追加

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,6 +14,6 @@ module AttendancesHelper
   
   # 出社と退社時間を受け取り、在社時間を計算して返します。
   def working_times(start, finish)
-    format("%.2f",((finish - start) / 60) / 60.0)
+    format("%.2f",(((finish - start) / 60) / 60.0))
   end
 end
