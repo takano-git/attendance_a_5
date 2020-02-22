4db19c6446860506108acff2dc1ae0d79a82668c
EC2 Default User <ec2-user@ip-172-31-42-221.ap-northeast-1.compute.internal>
2020-02-22 10:42:33 +0000

    GitHubにプッシュ済

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -29,7 +29,22 @@ class AppliesController < ApplicationController
   end
 
   def update_month
+    # if "1" == params[:user][:applies][:check]
+    # end
+    
+    ActiveRecord::Base.transaction do # トランザクションを開始します。
+      month_params.each do |id, item|
+        apply = Apply.find(id)
+        apply.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヶ月分の勤怠申請を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid # トランザクションによるエラーの分岐です。
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to applies_update_month_user_url(date: params[:date])
   end
+
   
     private
 
@@ -38,6 +53,6 @@ class AppliesController < ApplicationController
     end
     
     def month_params
-      params.require(:user).permit(applies: [:month, :mark, :authorizer, :id, :apply_id])[:applies]
+      params.require(:user).permit(applies: [:month, :mark, :check, :authorizer, :id, :apply_id])[:applies]
     end
 end
